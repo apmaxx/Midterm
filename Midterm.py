50c238b22b8809f2a81ff8b7f2032b111690214a
Amir <amirpacpett@gmail.com>
2025-03-12 15:38:30 -0400

    modified:   Midterm.py 	new file:   static/eightbites.jpeg 	new file:   static/hungergames.jpg 	new file:   static/midtermstyles.css 	new file:   static/monument14.jpg 	modified:   templates/amirsmidterm.html

--- Midterm.py
+++ Midterm.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route('/books')
 def book_list():
-    books= ["The Hunger Games by Jennifer Collins", 
+    books= ["The Hunger Games by Suzanne Collins", 
             "Monument 14 by Emmy Melbourne",
             "Eighth Grade Bites by Heather Brewer",
-            
-            ]
-
+             ]
+    
     return  render_template ("amirsmidterm.html", books=books)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
